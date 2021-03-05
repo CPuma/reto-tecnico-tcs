95a0fdf76f6f662d92eb4f2503b31a285964616c
Cesar Puma <puma.rasec@yahoo.es>
2021-03-05 02:43:06 -0500

    fix validacion de pagina en Personajes

--- star-wars/src/apiServices/people/people.controller.js
+++ star-wars/src/apiServices/people/people.controller.js
@@ -6,12 +6,12 @@ const getPeople = async (req, res, next) => {
 		const { _hostname } = req;
 
 		let { pagina, buscar } = req.query;
-		pagina = pagina ? parseInt((pagina || 0).toString(), 10) : undefined;
+		pagina = isNaN(pagina) ? undefined : parseInt(pagina, 10);
 		buscar = buscar ? buscar.toString() : undefined;
 
 		const people = await peopleService.getPeople(pagina, buscar);
 
-		return res.send(presenterPeople(people, _hostname));
+		return res.status(200).send(presenterPeople(people, _hostname));
 	} catch (error) {
 		next(error);
 	}
@@ -25,7 +25,7 @@ const getPersonWithId = async (req, res, next) => {
 
 		const person = await peopleService.getPersonWithId(id);
 
-		return res.send(presenterPerson(person, _hostname));
+		return res.status(200).send(presenterPerson(person, _hostname));
 	} catch (error) {
 		next(error);
 	}
