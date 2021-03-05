f9ada706307dfb56f4a0635e7cdadeec9a17c90a
Cesar Puma <puma.rasec@yahoo.es>
2021-03-05 02:50:07 -0500

    Fix get Pokemon by Id

--- dynamo-pokedex/src/handlers/get-pokemon.js
+++ dynamo-pokedex/src/handlers/get-pokemon.js
@@ -4,13 +4,12 @@ import { middyMiddleware } from '../middlewares/middy';
 const getPokemon = async (event, context) => {
 	try {
 		const {pokemonId} = event.pathParameters;
-
+		console.log(pokemonId)
 		const pokemonService = new PokemonService();
-		const pokemon = await pokemonService.get(pokemonId);
+		const {Item} = await pokemonService.get(pokemonId);
+		if(!Item) return { statusCode: 204 };
 
-		if(!pokemon) return { statusCode: 204 };
-
-		return { statusCode: 200, body: getPokemonPresenter(pokemon) };
+		return { statusCode: 200, body: getPokemonPresenter(Item) };
 	} catch (error) {
 		return { statusCode: 500, body: error.message };
 	}
